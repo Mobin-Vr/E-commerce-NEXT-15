10f6eb7d9b8d7d023954b15b452b81ba31a0b500
Mobin <Mobin.Vr@outlook.com>
2024-12-02 00:33:34 +0330

    final

---
+++ app/search/page.jsx
@@ -0,0 +1,7 @@
+async function page({ searchParams }) {
+   const { query } = await searchParams;
+
+   return <div>Search page</div>;
+}
+
+export default page;
---
+++ sanity/lib/live.js
@@ -0,0 +1,19 @@
+'use server-only';
+
+import { defineLive } from 'next-sanity';
+import { client } from '@/sanity/lib/client';
+
+// Set your viewer token
+const token = process.env.SANITY_API_READ_TOKEN;
+if (!token) {
+   throw new Error('Missing SANITY_API_READ_TOKEN');
+}
+
+export const { sanityFetch, SanityLive } = defineLive({
+   client,
+   serverToken: token,
+   browserToken: token,
+   fetchOptions: {
+      revalidate: 0,
+   },
+});
